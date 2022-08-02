f5ce5999a16198eed26ca371f5dbde29c3d8fc89
Szabolcs <szabolcs.beko@gmail.com>
2022-08-02 14:19:12 +0200

    First commit

---
+++ src/main/java/hu/home/springbootcrud/entity/Product.java
@@ -0,0 +1,33 @@
+package hu.home.springbootcrud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    @XmlElement(name = "id")
+    private int id;
+    @XmlElement(name = "name")
+    private String name;
+    @XmlElement(name = "quantity")
+    private int quantity;
+    @XmlElement(name = "price")
+    private double price;
+}
+
